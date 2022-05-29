d8dad14d9bccc37302106c677895e170da24f170
Viktor Pogrebniak <avtomaton@gmail.com>
2022-05-29 21:14:15 +0300

    curriculum: run evaluation thread

--- python/curriculum.py
+++ python/curriculum.py
@@ -4,10 +4,11 @@ import argparse
 import os
 import shutil
 import time
+from threading import Thread
 
 from sgfmill import sgf
 
-# format: victim name, condition for moving on
+# format: victim name, conditions for moving on: win rate, diff score, diff score without komi, policy_loss
 curriculum_conf = [
   ['kata1-b6c96-s41312768-d6061202.txt.gz', 75, None, None, None],
   ['kata1-b40c256-s7186724608-d1743537710.bin.gz', 75, None, None, None],
@@ -54,49 +55,6 @@ class PlayerStat:
         return True
     return False
 
-class Curriculum:
-  def __init__(self, config):
-    if len(config) < 1:
-      raise ValueError("Empty config for the curriculum play!")
-
-    self.victim_idx = 0
-    self.finished = False
-    self.victims = []
-    for line in config:
-      cond = PlayerStat(line[0], win_rate=line[1], score_diff=line[2], score_wo_komi_diff=line[3], policy_loss=line[4])
-      if not cond.can_be_victim_criteria():
-        raise ValueError("Incorrect victim change criteria for victim '{}': should be a single value enabled".format(line[0]))
-      self.victims.append(cond)
-
-  def cur_victim(self):
-    return self.victims[self.victim_idx]
-
-  def try_move_on(self, adv_stat, victims_input_dir, victims_output_dir):
-    if self.finished:
-      return
-
-    if not self.cur_victim().check_if_gt(adv_stat):
-      return
-
-    self.victim_idx += 1
-    if self.victim_idx == len(self.victims):
-      self.finished = True
-      return
-
-    print("Moving to the next victim '{}'".format(self.cur_victim().name))
-    num_efforts = 0
-    while num_efforts < 10:
-      try:
-        shutil.copy(os.path.join(victims_input_dir, self.cur_victim().name), victims_output_dir)
-        return
-      except:
-        print("Cannot copy victim '{}', maybe filesystem problem? Waiting 10 sec...".format(self.cur_victim().name))
-        num_efforts += 1
-        time.sleep(10)
-
-    if num_efforts == 10:
-      raise RuntimeError("Problem copying victim '{}', curriculum stopped".format(self.cur_victim().name))
-
 
 def get_game_info(sgf_str: str):
   sgf_game = sgf.Sgf_game.from_string(sgf_str)
@@ -104,18 +62,23 @@ def get_game_info(sgf_str: str):
   b_name = sgf_game.get_player_name('b')
   w_name = sgf_game.get_player_name('w')
 
-  victim_color = {b_name: 'b', w_name: 'w'}['victim']
+  if 'victim-' in b_name and '__' not in b_name:
+    # it has 'victim' in its name and it is not a colored evaluator
+    victim_name = b_name
+  else:
+    victim_name = w_name
+
+  victim_color = {b_name: 'b', w_name: 'w'}[victim_name]
   adv_color = {'b': 'w', 'w': 'b'}[victim_color]
 
   adv_raw_name = {'b': b_name, 'w': w_name}[adv_color]
 
   # currently the model name will not be just 'victim'
-  # but we really don't need adversary name yet so probably it does not matter
-  # if we don't extract more than win rate
+  # this code was NOT tested so commented out yet
   # adv_name = (
-  #   adv_raw_name.split("__victim")[0]
+  #   adv_raw_name.split('__' + victim_name)[0]
   #   if adv_color == "b"
-  #   else adv_raw_name.split("victim__")[1]
+  #   else adv_raw_name.split(victim_name + '__')[1]
   # )
   # adv_steps = (
   #   0
@@ -146,7 +109,7 @@ def get_game_info(sgf_str: str):
   return AdvGameInfo(winner, adv_minus_victim_score, adv_minus_victim_score_wo_komi)
 
 
-def recompute_statistics(selfplay_dir, games_for_compute):
+def recompute_statistics(selfplay_dir: str, games_for_compute: int):
   all_sgfs = []
   for (path, dirnames, filenames) in os.walk(selfplay_dir, followlinks=True):
     for f in filenames:
@@ -172,7 +135,7 @@ def recompute_statistics(selfplay_dir, games_for_compute):
 
   # don't have enough data
   if len(sgf_strings) < games_for_compute:
-    return
+    return None
 
   sum_wins = 0
   sum_loses = 0
@@ -196,6 +159,85 @@ def recompute_statistics(selfplay_dir, games_for_compute):
   return PlayerStat(win_rate=win_rate, score_diff=mean_diff_score, score_wo_komi_diff=mean_diff_score_wo_komi)
 
 
+class Curriculum:
+  def __init__(self, config, victims_input_dir, victims_output_dir):
+    if len(config) < 1:
+      raise ValueError("Empty config for the curriculum play!")
+
+    self.victims_input_dir = victims_input_dir
+    self.victims_output_dir = victims_output_dir
+
+    self.victim_idx = 0
+    self.finished = False
+    self.victims = []
+    for line in config:
+      cond = PlayerStat(line[0], win_rate=line[1], score_diff=line[2], score_wo_komi_diff=line[3], policy_loss=line[4])
+      if not cond.can_be_victim_criteria():
+        raise ValueError("Incorrect victim change criteria for victim '{}': should be a single value enabled".format(line[0]))
+      self.victims.append(cond)
+
+  def cur_victim(self):
+    return self.victims[self.victim_idx]
+
+  def try_move_on(self, adv_stat=None, policy_loss=None):
+    if self.finished:
+      return
+
+    print("Checking whether we need to move to the next victim...")
+    want_victim_update = False
+    if adv_stat is not None and self.cur_victim().check_if_gt(adv_stat):
+      want_victim_update = True
+    if policy_loss is not None:
+      raise NotImplementedError("Policy loss check is not implemented yet")
+
+    if not want_victim_update:
+      return
+
+    self.victim_idx += 1
+    if self.victim_idx == len(self.victims):
+      self.finished = True
+      return
+
+    print("Moving to the next victim '{}'".format(self.cur_victim().name))
+    num_efforts = 0
+    while num_efforts < 10:
+      try:
+        shutil.copy(os.path.join(self.victims_input_dir, self.cur_victim().name), self.victims_output_dir)
+        return
+      except:
+        print("Cannot copy victim '{}', maybe filesystem problem? Waiting 10 sec...".format(self.cur_victim().name))
+        num_efforts += 1
+        time.sleep(10)
+
+    if num_efforts == 10:
+      raise RuntimeError("Problem copying victim '{}', curriculum stopped".format(self.cur_victim().name))
+
+  def threaded_loop(
+          self,
+          selfplay_dir,
+          games_for_compute,
+          checking_periodicity):
+    while True:
+      adv_stat = recompute_statistics(selfplay_dir, games_for_compute)
+      if adv_stat is not None:
+        curriculum.try_move_on(adv_stat=adv_stat)
+        if curriculum.finished:
+          print("Curriculum is done. Stopping")
+          break
+      time.sleep(checking_periodicity)
+
+  def run_thread(
+          self,
+          selfplay_dir,
+          games_for_compute,
+          checking_periodicity):
+    thread = Thread(target=self.threaded_loop,
+                    args=(self, selfplay_dir, games_for_compute, checking_periodicity))
+    thread.start()
+    thread.join()
+    print("Curriculum thread finished!")
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Run victim replacement based on win rate.')
   parser.add_argument('-selfplay-dir', required=True, help='Directory with selfplay data')
@@ -206,11 +248,8 @@ if __name__ == '__main__':
 
   args = parser.parse_args()
 
-  curriculum = Curriculum(curriculum_conf)
-  while True:
-    adv_stat = recompute_statistics(args.selfplay_dif, args.games_for_compute)
-    curriculum.try_move_on(adv_stat, args.input_models_dir, args.output_models_dir)
-    if curriculum.finished:
-      print("Curriculum is done. Stopping")
-      break
-    time.sleep(args.checking_periodicity)
+  curriculum = Curriculum(curriculum_conf, args.input_models_dir, args.output_models_dir)
+  curriculum.run_thread(
+          args.selfplay_dir,
+          args.games_for_compute,
+          args.checking_periodicity)

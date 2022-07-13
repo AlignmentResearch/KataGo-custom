ab4a42ac37bd96836c11a6e0f6e24d0400ed01e4
Tony Wang <twang6@mit.edu>
2022-07-12 20:31:38 -0700

    Fix a weightSum <= 0.0 condition
    Not sure why this was not necessary before the refactor...

--- cpp/search/search.cpp
+++ cpp/search/search.cpp
@@ -1711,10 +1711,12 @@ void Search::recursivelyRecomputeStats(SearchNode& n) {
       double scoreMeanAvg = node->stats.scoreMeanAvg.load(std::memory_order_acquire);
       double scoreMeanSqAvg = node->stats.scoreMeanSqAvg.load(std::memory_order_acquire);
 
-      //It's possible that this node has 0 weight in the case where it's the root node
-      //and has 0 visits because we began a search and then stopped it before any playouts happened.
-      //In that case, there's not much to recompute.
-      if(weightSum <= 0.0) {
+      //It's possible that this node has 0 weight, there are two possibilities:
+      //  1. In the case where it's the root node and has 0 visits, it's because
+      //     we began a search and then stopped it before any playouts happened.
+      //     In that case, there's not much to recompute.
+      //  2. We could be doing EMCTS1.
+      if(weightSum <= 0.0 && searchParams.searchAlgo != SearchParams::SearchAlgorithm::EMCTS1) {
         assert(numVisits == 0);
         assert(isRoot);
       }

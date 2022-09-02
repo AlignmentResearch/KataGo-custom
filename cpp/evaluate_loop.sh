978ed303c5b34b4a0e2fdd42cecdf7afc0d84ed0
Nora Belrose <belrose.nora@gmail.com>
2022-09-02 12:55:25 -0700

    Eval loop shell script

---
+++ cpp/evaluate_loop.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -eu
+if [[ $# -ne 1 ]]
+then
+    echo "Usage: $0 BASE_DIR"
+    echo "Currently expects to be run from within the 'cpp' directory of the KataGo repo."
+    echo "BASE_DIR containing selfplay data and models and related directories"
+    exit 0
+fi
+
+BASE_DIR="$1"
+MODELS_DIR="$BASE_DIR"/models
+VICTIMS_DIR="$BASE_DIR"/victims
+OUTPUT_DIR="$BASE_DIR"/eval
+mkdir -p "$OUTPUT_DIR"/logs
+mkdir -p "$OUTPUT_DIR"/sgfs
+
+LAST_STEP=0
+while true
+do
+    # https://stackoverflow.com/questions/1015678/get-most-recent-file-in-a-directory-on-linux
+    VICTIM=$(ls -Art "$VICTIMS_DIR" | tail -n 1)
+    if [ -z "$VICTIM" ]
+    then
+        echo "No victim found, waiting..."
+        sleep 60
+        continue
+    fi
+
+    # Loop over directories in models/ in *natural sorted order* and see if any are new.
+    # This means that the numeric components of the directory names will be sorted based
+    # on their numeric value, not as strings, so early models will be evaluated first.
+    for MODEL_DIR in $(ls -v "$MODELS_DIR") ; do
+        if [[ "$MODEL_DIR" =~ -s([0-9]+) ]] ; then
+            # The first capture group is the step number
+            STEP=${BASH_REMATCH[1]}
+
+            # Have we evaluated this model yet?
+            if [ "$STEP" -gt "$LAST_STEP" ] ; then
+                # https://stackoverflow.com/questions/12152626/how-can-i-remove-the-extension-of-a-filename-in-a-shell-script
+                VICTIM_NAME=$(echo "$VICTIM" | cut -f 1 -d '.')
+
+                # Run the evaluation
+                echo "Evaluating model $MODEL_DIR against victim $VICTIM_NAME"
+                /engines/KataGo-custom/cpp/katago match \
+                    -config /go_attack/configs/match-1gpu.cfg \
+                    -override-config numGamesTotal=100 \
+                    -override-config nnModelFile0="$VICTIMS_DIR"/"$VICTIM" \
+                    -override-config nnModelFile1="$MODELS_DIR"/"$MODEL_DIR"/model.bin.gz \
+                    -sgf-output-dir "$OUTPUT_DIR"/sgfs/"$VICTIM_NAME"_"$MODEL_DIR" \
+                    2>&1 | tee "$OUTPUT_DIR"/logs/"$VICTIM_NAME"_"$MODEL_DIR".log
+
+                # Update the last step
+                LAST_STEP="$STEP"
+            fi
+        fi
+    done
+    sleep 30
+done

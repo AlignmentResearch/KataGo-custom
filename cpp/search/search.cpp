b7f317272e088542b0cb5fbd7a14f2468c504504
Tom Tseng <tom.hm.tseng@gmail.com>
2023-02-23 19:09:00 -0800

    fixes

--- cpp/search/search.cpp
+++ cpp/search/search.cpp
@@ -504,12 +504,14 @@ Search::Search(
 {
   if (searchParams.usingAdversarialAlgo()) {
     assert(searchParams.numThreads == 1); // We do not support multithreading with AMCTS (yet).
-    assert(oppNNEval != nullptr);
+    assert(oppNNEval != nullptr || searchParams.maxVisits == 1);
 
     oppParams.maxVisits = params.searchAlgo == SearchParams::SearchAlgorithm::AMCTS_R
       ? params.oppVisitsOverride.value_or(oppParams.maxVisits)
       : 1;
-    assert(!oppParams.usingAdversarialAlgo() || oppParams.maxVisits == 1);
+    // We don't want to recursively model an opponent also using A-MCTS or else
+    // we'll have infinite recursion.
+    assert(!oppParams.usingAdversarialAlgo() || searchParams.maxVisits == 1 || oppParams.maxVisits == 1);
     oppParams.rootNumSymmetriesToSample = params.searchAlgo == SearchParams::SearchAlgorithm::AMCTS_S ? 1 : oppParams.rootNumSymmetriesToSample;
     oppBot = make_unique<Search>(oppParams, oppNNEval, logger, rSeed + "-victim-model");
   }

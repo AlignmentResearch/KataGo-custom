8c1cad6eb127e8b45c3ee927a25e1553d2043a0a
Tom Tseng <tom.hm.tseng@gmail.com>
2023-07-20 19:48:04 -0700

    Distinguish different nodes with same move and parent move
    having MoveTrace list only `move` and `parent_move` means one
    `MoveTrace` item can represent several different board states. We almost
    certainly want different board states to have different nodes in the
    sankey diagram.

--- python/get_move_visits_from_analysis.py
+++ python/get_move_visits_from_analysis.py
@@ -4,7 +4,7 @@ import itertools
 import json
 from collections import Counter
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Iterable
 import plotly.graph_objects as go
 from collections import defaultdict
 #%%
@@ -31,11 +31,11 @@ To convert an SGF file to an `analysis` input JSON, use `sgf_to_analysis_input.p
 
 
 @dataclass(frozen=True)
-class MoveTrace:
-    """Indicates the move of an MCTS node and the move of the node's parent."""
-
+class MCTSNode:
+    """Indicates the move of an MCTS node and the node's parent."""
     move: str | None
-    parent_move: str | None
+    node_id: int
+    parent_node_id: int
 
 
 def sum_lists_elementwise(l1: list[Any], l2: list[Any]) -> list[Any]:
@@ -52,14 +52,14 @@ def sum_lists_elementwise(l1: list[Any], l2: list[Any]) -> list[Any]:
 
 
 def get_move_visit_stats(
-    data: dict[str, Any], parent_move: str | None = None
-) -> list[Counter[MoveTrace]]:
+    data: dict[str, Any],  node_id_generator: Iterable[int] = itertools.count(), parent_node_id: int | None = None) -> list[Counter[MCTSNode]]:
     """Returns visit counts for each move in the MCTS tree.
 
     Args:
         data: KataGo `analysis` JSON output describing an MCTS tree
         parent_move: The move used to reach the top-level node described by
           `data`.
+        node_id_generator: Iterator generating unique integers.
 
     Returns:
         A list L where L[i] contains depth-i moves and their associated visit
@@ -75,6 +75,7 @@ def get_move_visit_stats(
     move = data.get("move")
     visits = data.get("visits")
     children_data = data.get("children")
+    node_id = node_id_generator.__next__()
 
     # Handle the root node, which has different field names.
     if visits is None:
@@ -85,28 +86,27 @@ def get_move_visit_stats(
     child_stats = []
     for child_data in children_data:
         child_stats = sum_lists_elementwise(
-            child_stats, get_move_visit_stats(child_data, parent_move=move)
+            child_stats, get_move_visit_stats(child_data, node_id_generator=node_id_generator, parent_node_id=node_id)
         )
 
     stats = [
-        Counter({MoveTrace(move=move, parent_move=parent_move): visits})
+        Counter({MCTSNode(move=move, node_id=node_id, parent_node_id=parent_node_id): visits})
     ] + child_stats
     return stats
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(
-    #     description=DESCRIPTION, formatter_class=argparse.RawDescriptionHelpFormatter
-    # )
-    # parser.add_argument(
-    #     "files",
-    #     help="input files where each file consists of a JSON object per line",
-    #     nargs="+",
-    # )
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "files",
+        help="input files where each file consists of a JSON object per line",
+        nargs="+",
+    )
+    args = parser.parse_args()
 
-    # input_files = args.files
-    input_files = ["search_analysis_sgf_to_json/250-000.json"]
+    input_files = args.files
 
     for filename in input_files:
         print(f"\nFile {filename}")
@@ -126,51 +126,41 @@ if __name__ == "__main__":
 
 #%%
 label = ["root"]
-label_name_dict = {"root": 0}
+node_id_to_index = {"root": 0}
 x, y = [0.0], [0.5]
 target_counts = defaultdict(int)
 other_counts = [0 for _ in range(len(visit_stats) - 1)]
 source, target, value = [], [], []
 max_depth = len(visit_stats) - 1
-THRESHOLD = 3000
+THRESHOLD = 100
 for depth, depth_stats in enumerate(visit_stats[1:]):
     label.append(f"{depth}: other")
-    label_name_dict[f"{depth}: other"] = len(label) - 1
+    node_id_to_index[f"{depth}: other"] = len(label) - 1
     x.append((depth + 1)/ (max_depth + 0.1))
     y.append(0.0001)
 
-    for index, (move_trace, count) in enumerate(depth_stats.items()):
-        label_name = f"{depth}: {move_trace.move}"
-        if label_name not in label_name_dict:
-            label.append(label_name)
-            label_name_dict[label_name] = len(label) - 1
+    for index, (node, count) in enumerate(depth_stats.items()):
+        if node.node_id not in node_id_to_index:
+            label.append(f"{depth}: {node.move}")
+            node_id_to_index[node.node_id] = len(label) - 1
             x.append((depth + 1)/ (max_depth + 0.1))
             y.append((index + 1) / (len(depth_stats) + 3))
 
-    for move_trace, count in depth_stats.items():
-        parent, child = move_trace.parent_move, move_trace.move
-        if child is None:
-            continue
-        child_label_name = f"{depth}: {child}"
-        target_counts[child_label_name] += count
+    for node, count in depth_stats.items():
+        target_counts[node.node_id] += count
 
-    for move_trace, count in depth_stats.items():
-        parent, child = move_trace.parent_move, move_trace.move
-        if child is None:
-            continue
-        if parent is None:
-            parent_label_name = "root"
-        else:
-            parent_label_name = f"{depth - 1}: {parent}"
-            parent_target_count = target_counts[parent_label_name]
-            parent_label_name = parent_label_name if parent_target_count > THRESHOLD else f"{depth - 1}: other"
+    for node, count in depth_stats.items():
+        node_id = node.node_id
+        parent_node_id = node.parent_node_id
+        if parent_node_id == 0:
+            parent_node_id = "root"
+        elif target_counts[parent_node_id] <= THRESHOLD:
+            parent_node_id = f"{depth - 1}: other"
+        if target_counts[node_id] <= THRESHOLD:
+            node_id = f"{depth}: other"
 
-        child_label_name = f"{depth}: {child}"
-        child_target_count = target_counts[child_label_name]
-        child_label_name = child_label_name if child_target_count > THRESHOLD else f"{depth}: other"
-
-        source.append(label_name_dict[parent_label_name])
-        target.append(label_name_dict[child_label_name])
+        source.append(node_id_to_index[parent_node_id])
+        target.append(node_id_to_index[node_id])
         value.append(count)
     
 print("label x,y", list(zip(label,zip(x, y))))

c8c990d308f6c49bdbadcc910aa0b073d6bedb6f
Tom Tseng <tom@alignmentfund.org>
2023-10-23 15:58:09 -0700

    plot_losses: allow plotting validation loss

--- python/plot_losses.py
+++ python/plot_losses.py
@@ -13,16 +13,23 @@ def main():
     parser.add_argument(
         "-output", help="Path to write the loss plot", type=Path, required=True
     )
+    parser.add_argument(
+        "--validation",
+        help="Print validation loss instead of train loss",
+        action="store_true",
+    )
     args = parser.parse_args()
 
     for d in args.dirs:
-        train_metrics_file = d / "train" / "t0" / "metrics_train.json"
+        file_modifier = "val" if args.validation else "train"
+        metrics_file = d / "train" / "t0" / f"metrics_{file_modifier}.json"
+        nsamp_key = "nsamp_train" if args.validation else "nsamp"
         nsamps = []
         losses = []
-        with open(train_metrics_file) as f:
+        with open(metrics_file) as f:
             for line in f:
                 metrics = json.loads(line)
-                nsamps.append(metrics["nsamp"])
+                nsamps.append(metrics[nsamp_key])
                 losses.append(metrics["loss"])
         plt.plot(nsamps, losses, label=d.name)
 

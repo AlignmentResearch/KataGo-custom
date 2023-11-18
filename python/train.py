762da778967b1139104f5169b5dc663146c64060
Tom Tseng <tomtseng@users.noreply.github.com>
2023-11-17 16:24:20 -0800

    train.py: Add -disable-vtimeloss to PyTorch training (#98)
    With the old TF training code we had a flag `-disable-vtimeloss` to disable the variance time loss. We removed it when we moved to KataGo v1.12.0, which changes the training code to PyTorch, since we didn't want to add extraneous changes to the PyTorch code and we weren't using the PyTorch code.
    
    This PR adds the `-disable-vtimeloss` flag back.
    
    Testing: Checked that running `train.py` with `-disable-vtimeloss` drives the loss down without driving vtimeloss down.

--- python/train.py
+++ python/train.py
@@ -91,6 +91,8 @@ if __name__ == "__main__":
   parser.add_argument('-intermediate-loss-scale', type=float, help='Loss factor scale for intermediate head', required=False)
   parser.add_argument('-intermediate-distill-scale', type=float, help='Distill factor scale for intermediate head', required=False)
 
+  parser.add_argument('-disable-vtimeloss', help='Disable vtimeloss for training', required=False, dest="use_vtimeloss", action='store_false')
+
   args = vars(parser.parse_args())
 
 
@@ -169,6 +171,8 @@ def main(rank: int, world_size: int, args, multi_gpu_device_ids, readpipes, writ
   main_loss_scale = args["main_loss_scale"]
   intermediate_loss_scale = args["intermediate_loss_scale"]
   intermediate_distill_scale = args["intermediate_distill_scale"]
+
+  use_vtimeloss = args["use_vtimeloss"]
 
   if lr_scale is None:
     lr_scale = 1.0
@@ -944,6 +948,7 @@ def main(rank: int, world_size: int, args, multi_gpu_device_ids, readpipes, writ
           main_loss_scale=main_loss_scale,
           intermediate_loss_scale=intermediate_loss_scale,
           intermediate_distill_scale=intermediate_distill_scale,
+          use_vtimeloss=use_vtimeloss,
         )
 
         # DDP averages loss across instances, so to preserve LR as per-sample lr, we scale by world size.
@@ -1150,6 +1155,7 @@ def main(rank: int, world_size: int, args, multi_gpu_device_ids, readpipes, writ
               main_loss_scale=main_loss_scale,
               intermediate_loss_scale=intermediate_loss_scale,
               intermediate_distill_scale=intermediate_distill_scale,
+              use_vtimeloss=use_vtimeloss,
             )
             metrics = detensorify_metrics(metrics)
             accumulate_metrics(val_metric_sums, val_metric_weights, metrics, batch_size, decay=1.0, new_weight=1.0)
